179b9e92d8b187d3ab25634e0a6695a460672f7e
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-04-02 23:18:20 +0200

    Implemented words service

--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { WORDS } from '../data/data-base';
+import { WordType } from '../data/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordsService {
+  private words: WordType[] = [];
+  private nouns: WordType[] = [];
+  private verbs: WordType[] = [];
 
-  constructor() { }
+  constructor() {
+    this.words = WORDS;
+  }
+
+  getWords(): WordType[] {
+    return this.words;
+  }
+
+  getNouns(): WordType[] {
+    return this.nouns;
+  }
+
+  getVerbs(): WordType[] {
+    return this.verbs;
+  }
+
+  addNoun(value: WordType): void {
+    this.verbs.push(value);
+  }
+
+  addVerb(value: WordType): void {
+    this.verbs.push(value);
+  }
 }
